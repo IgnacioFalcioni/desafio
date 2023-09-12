7471b51e62c58c8563972b4cd5cf9f0d29ce2459
Ignacio Falcioni <ignaciofalcioni.1@gmail.com>
2023-09-12 15:47:36 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+class Product {
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+    this.id = null; 
+  }
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.productIdCounter = 1; 
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  addProduct(product) {
+    
+    const codeExists = this.products.some((p) => p.code === product.code);
+
+    if (codeExists) {
+      throw new Error("El código del producto ya existe.");
+    }
+
+    
+    product.id = this.productIdCounter++;
+    this.products.push(product);
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+      throw new Error("Producto no encontrado");
+    }
+    return product;
+  }
+}
+
+
+const productManager = new ProductManager();
+
+
+const initialProducts = productManager.getProducts();
+console.log("Productos iniciales:", initialProducts);
+
+
+try {
+  const newProduct = new Product("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+  productManager.addProduct(newProduct);
+  console.log("Producto agregado:", newProduct);
+} catch (error) {
+  console.error(error.message);
+}
+
+
+const productsAfterAdd = productManager.getProducts();
+console.log("Productos después de agregar:", productsAfterAdd);
+
+
+try {
+  const duplicateProduct = new Product("Producto duplicado", "Este es un producto duplicado", 300, "Sin imagen", "abc123", 30);
+  productManager.addProduct(duplicateProduct);
+  console.log("Producto duplicado agregado:", duplicateProduct);
+} catch (error) {
+  console.error(error.message);
+}
+
+
+const foundProduct = productManager.getProductById(1);
+console.log("Producto encontrado por ID:", foundProduct);
+
+
+try {
+  const nonExistentProduct = productManager.getProductById(999);
+  console.log("Producto no existente encontrado:", nonExistentProduct);
+} catch (error) {
+  console.error(error.message);
+}
+
