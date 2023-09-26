36cc610e1bfa4cf00ac0ace90fa4dc916b2b8a7e
Ignacio Falcioni <ignaciofalcioni.1@gmail.com>
2023-09-26 09:47:56 -0300

    agregue express

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const fs = require('fs');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-export default App;
+
+app.get('/products', (req, res) => {
+  try {
+    const data = fs.readFileSync('productos.json', 'utf8');
+    const products = JSON.parse(data);
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los productos' });
+  }
+});
+
+
+app.get('/products/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+
+  try {
+    const data = fs.readFileSync('productos.json', 'utf8');
+    const products = JSON.parse(data);
+    const product = products.find((p) => p.id === productId);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el producto' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express en funcionamiento en el puerto ${PORT}`);
+});
